defd14867ee73bd5c89fb28cfc9c04f252f8779b
Vadym Bartko <hudvin@gmail.com>
2016-07-18 17:15:27 +0300

    remove proj

--- proj/src/app/index.module.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  'use strict';
-
-  angular
-    .module('proj', ['angularFileUpload','ngAnimate', 'ngCookies', 'ngTouch', 'ngSanitize', 'ngMessages', 'ngAria',
-      'ngResource', 'ui.router', 'ui.bootstrap', 'toastr'
-      ]);
-
-})();
